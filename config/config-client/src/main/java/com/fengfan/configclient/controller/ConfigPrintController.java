1aefffa883185f32c4f2ffce6ceca6172a49e6be
fengfan <15202849186@163.com>
2022-05-30 14:05:53 +0800

    配置中心的使用

---
+++ config/config-client/src/main/java/com/fengfan/configclient/controller/ConfigPrintController.java
@@ -0,0 +1,25 @@
+package com.fengfan.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fengfan
+ * @description 打印配置服务中的配置内容
+ * @date 2022/5/30 9:37
+ */
+@RestController
+@RequestMapping("/api")
+public class ConfigPrintController {
+    @Value("${config.name}")
+    private String name;
+    @Value("${config.age}")
+    private String age;
+
+    @PostMapping("/printConfig")
+    public String printConfig(){
+        return "name：" + name + "，age：" + age;
+    }
+}
