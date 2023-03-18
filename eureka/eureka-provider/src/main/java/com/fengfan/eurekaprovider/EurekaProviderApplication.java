30ec6bec7ee861a670ed3fed0cbef75b48e0b150
fengfan <15202849186@163.com>
2023-03-18 18:15:19 +0800

    eureka配置

--- eureka/eureka-provider/src/main/java/com/fengfan/eurekaprovider/EurekaProviderApplication.java
+++ eureka/eureka-provider/src/main/java/com/fengfan/eurekaprovider/EurekaProviderApplication.java
@@ -2,10 +2,12 @@ package com.fengfan.eurekaprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient // 本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient
 public class EurekaProviderApplication {
 
     public static void main(String[] args) {
--- eureka/eureka-provider/src/main/java/com/fengfan/eurekaprovider/controller/ProviderController.java
+++ eureka/eureka-provider/src/main/java/com/fengfan/eurekaprovider/controller/ProviderController.java
@@ -1,11 +1,15 @@
 package com.fengfan.eurekaprovider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @author fengfan
@@ -15,9 +19,22 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/api")
 public class ProviderController {
+    @Autowired
+    private DiscoveryClient client;
 
     @Value("${server.port}")
     private String serverPort;
+
+    @PostMapping("/discovery")
+    public Object discovery(){
+        List<String> list = client.getServices();
+        System.out.println(list);
+        List<ServiceInstance> serviceInstances = client.getInstances("EUREKA-PROVIDER");
+        for (ServiceInstance serviceInstance: serviceInstances){
+            System.out.println(serviceInstance);
+        }
+        return client;
+    }
 
     @PostMapping("/sayHello")
     public String sayHello(String name) {
